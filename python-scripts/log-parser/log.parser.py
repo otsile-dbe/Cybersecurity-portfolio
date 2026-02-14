a82e21f7c8e60b79b4bd7f5bd87750942cb03cde
Palesa, Otsile <khanyipalesadube@icloud.com>
2026-02-14 09:45:02 +0200

    Add files via upload

---
+++ python-scripts/log-parser/log.parser.py
@@ -0,0 +1,4 @@
+"Simple Python script that scans a log file for a specific event keyword and counts how many times it occurs..."
+print(
+    "Enter the path to the log file you want to analyze (e.g., /var/log/syslog): "
+)
